eaf613a69e907bf64875c5c54dcc14ab96116b70
Tyler Langlois <tjl@byu.net>
2015-11-25 22:27:12 -0500

    utils: add a bare-bones beats packaging update script

---
+++ util/beats_release.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+dl_base=https://download.elastic.co/beats
+ret=0
+
+if [ $# -ne 1 ] ; then
+    echo "Usage: $0 <version>"
+    exit 1
+else
+    ver=$1
+fi
+
+pushd `dirname $0`/.. &>/dev/null
+
+# Update precompiled binary releases
+for bin_release in *beat-bin ; do
+    beat=${bin_release%%-bin}
+    artifact_32="$beat-$ver-i686.tar.gz"
+    artifact_64="$beat-$ver-x86_64.tar.gz"
+    checksums="$(mktemp /tmp/$(basename $0)-$beat.XXXXXX)"
+    curl -s $dl_base/$beat/$artifact_32.sha1.txt >> $checksums
+    curl -s $dl_base/$beat/$artifact_64.sha1.txt >> $checksums
+
+    pushd /tmp/
+
+    curl -s $dl_base/$beat/$artifact_32 -O
+    curl -s $dl_base/$beat/$artifact_64 -O
+
+    if gsha1sum -c "$checksums" ; then
+        checksum_32="$(gsha256sum $artifact_32 | awk '{ print $1; }')"
+        checksum_64="$(gsha256sum $artifact_64 | awk '{ print $1; }')"
+        popd
+        new_pkgbuild="$(sed -E \
+            -e "s/(sha256sums_i686=).*/\1('$checksum_32')/" \
+            -e "s/(sha256sums_x86_64=).*/\1('$checksum_64')/" \
+            $bin_release/PKGBUILD)"
+        echo "==> DIFF:"
+        diff $bin_release/PKGBUILD <(echo "$new_pkgbuild")
+        echo "==> FILES:"
+        tar tzvf /tmp/$artifact_32
+        echo -n "Write new PKGBUILD? (y/N): "
+        read want_creds
+
+        if [ $want_creds == 'y' ] ; then
+            echo "$new_pkgbuild" > $bin_release/PKGBUILD
+        fi
+        pushd /tmp/
+    else
+        ret=1
+        echo "Checksum for $beat failed, skipping..."
+    fi
+
+    rm $checksums $artifact_32 $artifact_64
+    popd
+
+done
+
+# Update from-source tarballs
+# TODO: No tagged 1.0.0 GA release exists in GitHub yet, update this when
+# there is one.
+# for src_release in *beat ; do
+# done
+
+popd
+exit $ret
